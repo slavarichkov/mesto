f07c0341a753ed820d72fc407e1659fbcedf413e
Slava Richkov <v.v.richkov@yandex.ru>
2022-05-23 23:37:07 +0500

    fix: корректировка полей ПИ

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const editButton = document.querySelector('.Edit-Button')
 const popup = document.querySelector('.popup')
 const saveButton = document.querySelector('.button')
 const closeIkon = document.querySelector('.closeIkon')
-const profileFirstname = document.querySelector('.profile-Info__firstname')
+const profileFirstname = document.querySelector('.profile__firstname')
 const firstnameInput = document.querySelector('.form__input_firstname')
 const professionInput = document.querySelector('.form__input_profession')
-const profileSubtext = document.querySelector('.profile-Info__subtext')
+const profileSubtext = document.querySelector('.profile__subtext')
 
 editButton.addEventListener('click', function() {
     popup.classList.add('popup_open')
