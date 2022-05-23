3e57ea7136fcce57e7723d437dbb462225769046
Slava Richkov <v.v.richkov@yandex.ru>
2022-05-23 21:37:37 +0500

    feat: отправить пр4

---
+++ index.js
@@ -0,0 +1,26 @@
+const editButton = document.querySelector('.Edit-Button') 
+const popup = document.querySelector('.popup')
+const saveButtom = document.querySelector('.form__buttom')
+const closeIkon = document.querySelector('.closeIkon')
+const profileFirstname = document.querySelector('.profile-Info__firstname')
+const firstnameInput = document.querySelector('.form__input_firstname')
+const professionInput = document.querySelector('.form__input_profession')
+const profileSubtext = document.querySelector('.profile-Info__subtext')
+
+editButton.addEventListener('click', function() {
+    popup.classList.add('popup_open')
+    firstnameInput.value = profileFirstname.textContent;
+    professionInput.value = profileSubtext.textContent;
+})
+
+saveButtom.addEventListener('click', function() {
+    popup.classList.remove('popup_open')
+    profileFirstname.textContent = firstnameInput.value;
+    profileSubtext.textContent = professionInput.value;
+})
+
+closeIkon.addEventListener('click', function() {
+    popup.classList.remove('popup_open')
+})
+
+console.log(profileFirstname)
