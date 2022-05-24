0626c33bbb269616251919bbf06cfc77bf5e606a
Slava Richkov <v.v.richkov@yandex.ru>
2022-05-24 20:15:28 +0500

    fix: автопроверка

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const editButton = document.querySelector('.profile__edit-button') 
+const editButton = document.querySelector('.profile__edit-button')
 const popup = document.querySelector('.popup')
 const saveButton = document.querySelector('.popup__button')
 const closeIkon = document.querySelector('.popup__closeikon')
@@ -7,20 +7,18 @@ const firstnameInput = document.querySelector('.popup__input_firstname')
 const professionInput = document.querySelector('.popup__input_profession')
 const profileSubtext = document.querySelector('.profile__subtext')
 
-editButton.addEventListener('click', function() {
+editButton.addEventListener('click', function () {
     popup.classList.add('popup_open')
     firstnameInput.value = profileFirstname.textContent;
     professionInput.value = profileSubtext.textContent;
 })
 
-saveButton.addEventListener('click', function() {
+saveButton.addEventListener('click', function () {
     popup.classList.remove('popup_open')
     profileFirstname.textContent = firstnameInput.value;
     profileSubtext.textContent = professionInput.value;
 })
 
-closeIkon.addEventListener('click', function() {
+closeIkon.addEventListener('click', function () {
     popup.classList.remove('popup_open')
 })
-
-console.log(profileFirstname)
