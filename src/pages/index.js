ffab25a4106f19ae4b6139726f11cb3c32421d9b
Slava Richkov <v.v.richkov@yandex.ru>
2022-08-02 22:22:57 +0500

    refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Card from "../scripts/Card.js";
-import FormValidator from "../scripts/FormValidator.js";
-import Section from "../scripts/Section.js";
-import PopupWithForm from "../scripts/PopupWithForm.js";
-import PopupWhithImage from "../scripts/PopupWhithImage.js";
-import UserInfo from "../scripts/UserInfo.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWhithImage from "../components/PopupWhithImage.js";
+import UserInfo from "../components/UserInfo.js";
 
 const buttonEdit = document.querySelector('.profile__edit-button');
 //const popupUserInput = document.querySelector('.popup_user_input');
